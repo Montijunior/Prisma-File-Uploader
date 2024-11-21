6f48de55de1f094197f559df5437df32d3dbcfda
Monti junior <montiabouou@gmail.com>
2024-11-21 02:18:45 -0800

    folder creation and viewing logic fixed

--- routes/FolderRoutes.js
+++ routes/FolderRoutes.js
@@ -2,6 +2,9 @@ const { Router } = require("express");
 const FolderController = require("../controllers/FolderController");
 const folderRouter = Router();
 
+// Get method to open a single folder
+folderRouter.get("/:id", FolderController.get_view_folder);
+
 // Post method to create new folders
 folderRouter.post("/create", FolderController.post_create_folder);
 
