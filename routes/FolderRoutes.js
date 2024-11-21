d2ad1d58b2bbd514a6029a7203bd557430e0def9
Monti junior <montiabouou@gmail.com>
2024-11-21 11:20:01 -0800

    added folder crud routes

--- routes/FolderRoutes.js
+++ routes/FolderRoutes.js
@@ -2,10 +2,16 @@ const { Router } = require("express");
 const FolderController = require("../controllers/FolderController");
 const folderRouter = Router();
 
+// Post method to create new folders
+folderRouter.post("/create", FolderController.post_create_folder);
+
 // Get method to open a single folder
 folderRouter.get("/:id", FolderController.get_view_folder);
 
-// Post method to create new folders
-folderRouter.post("/create", FolderController.post_create_folder);
+// Post method to edit folder
+folderRouter.post("/:id/edit", FolderController.post_edit_folder);
+
+// Post method to delete folders
+folderRouter.post("/:id/delete", FolderController.post_delete_folder);
 
 module.exports = folderRouter;
