04cc101eee9127fd584bf0dcbddaae3e4a193d02
Monti junior <montiabouou@gmail.com>
2024-11-21 00:05:22 -0800

    add poppins font to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{ejs,js}","!./node_modules/**/*","./controllers/**/*"],
+  content: ["./**/*.{ejs,js}", "!./node_modules/**/*", "./controllers/**/*"],
   theme: {
-    extend: {},
+    extend: { fontFamily: ["Poppins", "sans-serif"] },
   },
   plugins: [],
-}
-
+};
